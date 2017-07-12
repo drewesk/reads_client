449cbd66c7abc5289e675ae248140d3c29997196
Andrew Eskenazi <drewesk@gmail.com>
2017-07-11 20:14:15 -0600

    DRY’ed up my code!, shared functions refactor.

--- assets/JS/shared.js
+++ assets/JS/shared.js
@@ -5,14 +5,62 @@ $.ajaxSetup({
   }
 });
 
-const API_URL = ifLocalhost();
+const API_URL = 'https://reads-server.herokuapp.com';
 
-function ifLocalhost() {
-  let localHost = window.location.host.indexOf('localhost') != -1;
+/////////////////////////////////////////////
 
-  // if (localHost) {
-  //   return 'http://localhost:3000';
-  // } else {
-    return 'https://reads-server.herokuapp.com';
-  // }
+// const addAuthors = addEntity(getAuthors(), '#authors-template', '.authors-render');
+// const addBooks = addEntity(getBooks(), '#books-template', '.books-render');
+//
+// const getAuthors = getEntity('authors');
+// const getBooks = getEntity('books');
+// function declarations
+
+
+// function ifLocalhost() {
+//   // let localHost = window.location.host.indexOf('localhost') != -1;
+//     return ; //Ask CJ for a better refactor
+// }
+/////////////////////////////////////////////
+
+function getAuthors () {
+  return $.get(`${API_URL}/authors`);
 }
+
+function getBooks () {
+  return $.get(`${API_URL}/books`);
+}
+
+function addAuthors (authors) {
+  const source = $('#authors-template').html();
+  const template = Handlebars.compile(source);
+  const Authors = {authors};
+  const HTML = template(Authors);
+  console.log(HTML);
+  $('.authors-render').html(HTML);
+}
+
+function addBooks (books) {
+  const source = $('#books-template').html();
+  const template = Handlebars.compile(source);
+  const Books = {books};
+  const HTML = template(Books);
+  console.log(HTML);
+  $('.books-render').html(HTML);
+}
+
+/////////////////////////////////////////////
+
+// function getEntity(path) {
+//   return $.get(`${API_URL}/${path}`);
+// }
+//
+// function addEntity (entity, id, element) {
+//   const source = $(id).html();
+//   const template = Handlebars.compile(source);
+//   const Entity = {entity};
+//   const entHTML = template(Entity);
+//   console.log(entHTML);
+//   $(element).html(entHTML);
+// }
+/////////////////////////////////////////////
